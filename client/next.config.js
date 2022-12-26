ebccaa6c475a56899bf435dbbdab5ed765316ab1
백은지 <baeg-eunji@baeg-eunjiui-MacBookAir.local>
2022-12-26 21:27:27 +0900

    commit

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-52-79-227-34.ap-northeast-2.compute.amazonaws.com",
+      "ec2-43-201-103-253.ap-northeast-2.compute.amazonaws.com",
     ],
   },
   swcMinify: true,
